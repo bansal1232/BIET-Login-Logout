46f215db23f855ce7dc582598a6e9e03433b006b
bansal1232 <shubhambansal1232@gmail.com>
2017-11-14 19:03:11 +0530

    Add files via upload

---
+++ logout.py
@@ -0,0 +1,19 @@
+import requests,time
+from bs4 import BeautifulSoup
+
+def biet_logout(username,password):
+	try:
+		r=requests.post('http://172.16.40.5:8090/httpclient.html',data={'mode':'193','username':username,'password':password})
+		
+		soup=BeautifulSoup(r.content,'lxml-xml')
+		print(soup.find('message').text,flush=True)
+
+	except:
+		print("Something might wrong! Please check your internet connection",flush=True)
+	time.sleep(0.3)
+	
+if __name__ == '__main__':
+	#Write down the registered username and password 
+	username = ""
+	password = ""
+	biet_logout(username, password)
